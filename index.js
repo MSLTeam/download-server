89d9b12323d7032dc50e993d16226b6ba9a45808
luluxiaoyu <ljy.xiaoyu@qq.com>
2024-02-25 16:53:01 +0800

    ref: 重构下载模块 perf: 修改url拼接

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
-
+const request = require('request-promise');
 const app = express();
 app.use(express.json());
 
 const AUTH_TOKEN = 'your_auth_token'; // 授权令牌
 const SERVER_PORT = 3002; // 服务器端口
 const TEMP_DIR = 'temp'; // 临时文件夹
-const serverUrl = ''
+const serverUrl = '' //服务器url，若用非标准http端口，要加上
 
 // 检查临时文件夹是否存在，如果不存在则创建它
 if (!fs.existsSync(TEMP_DIR)) {
@@ -37,6 +36,7 @@ app.use((req, res, next) => {
     // 继续执行下一个中间件
     next();
 });
+
 app.use('/temp', express.static(path.join(__dirname, 'temp')));
 app.post('/', async (req, res) => {
     const fileUrl = req.body.url;
@@ -49,30 +49,22 @@ app.post('/', async (req, res) => {
     try {
         console.log(`请求下载的地址: ${fileUrl}`); // 打印请求的下载地址
 
-        const response = await axios.get(fileUrl, { responseType: 'stream' });
+        const response = await request.get(fileUrl, { encoding: null, timeout: 5000 });
         const randomPrefix = crypto.randomBytes(4).toString('hex');
         const filePath = path.join(TEMP_DIR, `${randomPrefix}_${path.basename(fileUrl)}`);
-        const writer = fs.createWriteStream(filePath);
 
-        response.data.pipe(writer);
+        fs.writeFileSync(filePath, response);
 
-        writer.on('finish', () => {
-            console.log(`下载完成: ${serverUrl}:${SERVER_PORT}/${filePath}`); // 打印下载完成的文件地址
+        console.log(`下载完成: ${serverUrl}/${filePath}`); // 打印下载完成的文件地址
 
-            res.status(200).send({ url: `${serverUrl}:${SERVER_PORT}/${filePath}`, message: '下载完成' });
+        res.status(200).send({ url: `${serverUrl}/${filePath}`, message: '下载完成' });
 
-            // 在2小时后删除文件
-            setTimeout(() => {
-                fs.unlink(filePath, err => {
-                    if (err) console.error(`无法删除文件: ${err}`);
-                });
-            }, 2 * 60 * 60 * 1000);
-        });
-
-        writer.on('error', (err) => {
-            res.status(500).send('镜像服务器下载文件失败');
-            console.error(`文件下载失败: ${err}`);
-        });
+        // 在2小时后删除文件
+        setTimeout(() => {
+            fs.unlink(filePath, err => {
+                if (err) console.error(`无法删除文件: ${err}`);
+            });
+        }, 2 * 60 * 60 * 1000);
     } catch (err) {
         res.status(500).send('镜像服务器下载文件失败');
         console.error(`文件下载失败: ${err}`);
